3b79ab2494b952d976a6293d5b27bf52df7e2c90
Chen Jo Wei <meettheworldvivc@gmail.com>
2017-04-09 16:31:50 +0800

    增加後台user路徑

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,53 @@
+class Admin::UsersController < ApplicationController
+
+	before_action :authenticate_user!
+	before_action :check_admin
+	before_action :find_user, only:[:show,:edit, :update]
+	layout "admin"
+
+	def index 
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+    	respond_to do |format|
+				  format.html { redirect_to admin_user_path(@user)}
+			end	
+    	
+    else
+    	render :edit
+
+    end
+  end
+
+
+
+
+
+
+	protected 
+	def check_admin 
+		unless current_user.role == "admin"
+			raise ActiveRecord::RecordNotFound			
+		end
+	end
+	def find_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name,:role)
+	end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 	namespace :admin do
   	resources :products  #後台CRUD都可
   	resources :npos 
+  	resources :users
 	end
 
 end
