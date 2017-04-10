466b4545a15f1918231955690efcfa8e35cfba31
Chris <tw.chrislyu@gmail.com>
2017-04-10 21:49:46 +0800

    CRUD users API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,40 @@
 class Api::V1::UsersController < ApiController
   respond_to :json
-
+  
   def show
     respond_with User.find(params[:id])
+  end
+
+  def create
+    @user=User.new(user_params) 
+      # if the user is saved successfully than respond with json data and status code 201
+    if @user.save 
+      render json: @user, status: 201
+    else
+      render json: { errors: @user.errors}, status: 422
+    end
+  end
+
+  # Updating users
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render json: @user, status: 200
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
+   # Deleting users
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   namespace :api, defaults:{ format: :json }do
     namespace :v1 do
       #devise_for :users
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end 
 
