9f6033092011e81ac2932ec98ded15c427e26fd9
Chen Jo Wei <meettheworldvivc@gmail.com>
2017-04-13 11:29:20 +0800

    fix array issue

---
+++ db/migrate/20170413031059_add_columns_on_donations_and_products.rb
@@ -0,0 +1,6 @@
+class AddColumnsOnDonationsAndProducts < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :products, :lottery_old_number, :text, array:true
+  	add_column :donations, :lottery_number, :text, array:true
+  end
+end
