fef91ef3046fc260912f61319647f03f43d0e34b
Chen Jo Wei <meettheworldvivc@gmail.com>
2017-04-18 12:32:48 +0800

    fix gemfile lock conflict

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module LuckyBuy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = "zh-TW"
+
 
   end
 end
