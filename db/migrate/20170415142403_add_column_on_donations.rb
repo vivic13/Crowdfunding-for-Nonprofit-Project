87ceb361004629cc8521a1b3aaccdbbea022d248
lyuchris <tw.chrislyu@gmail.com>
2017-04-19 14:27:34 +0800

    fix Mysql2 Error default value

--- db/migrate/20170415142403_add_column_on_donations.rb
+++ db/migrate/20170415142403_add_column_on_donations.rb
@@ -1,5 +1,5 @@
 class AddColumnOnDonations < ActiveRecord::Migration[5.0]
   def change
-  	add_column :donations, :address, :text, default: null
+  	add_column :donations, :address, :text, :default => nil, :null => true
   end
 end
