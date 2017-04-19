02459d23f1ea4d3c91ad35171905d1bfb66154d7
lyuchris <tw.chrislyu@gmail.com>
2017-04-19 14:10:09 +0800

    註解migrate

--- db/migrate/20170412054227_adjust_columns_on_products.rb
+++ db/migrate/20170412054227_adjust_columns_on_products.rb
@@ -1,6 +1,6 @@
 class AdjustColumnsOnProducts < ActiveRecord::Migration[5.0]
   def change
-  	remove_column :products, :amount
+  	# remove_column :products, :amount
   	add_column :products , :unit , :integer
   	add_column :products , :unit_price , :integer
   	add_column :products , :due_date , :date
