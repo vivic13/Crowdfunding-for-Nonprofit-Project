542f7321b5adddac3bcb9a016f105aaa520ec7d5
lyuchris <tw.chrislyu@gmail.com>
2017-05-01 00:42:43 +0800

    Project upload report function

--- app/controllers/admin/project_reports_controller.rb
+++ app/controllers/admin/project_reports_controller.rb
@@ -1,8 +1,45 @@
 class Admin::ProjectReportsController < ApplicationController
 	before_action :find_project
+
+	def show
+    @report = @project.report
+  end
+
 	def new
     @report = @project.build_report
   end
+
+  def create
+    @report = @project.build_report( report_params )
+    if @report.save
+      redirect_to admin_project_report_url( @project )
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @report = @project.report
+  end
+
+  def update
+    @report = @project.report
+
+    if @report.update( report_params )
+      redirect_to admin_project_report_url( @project )
+    else
+      render :action => :edit
+    end
+
+  end
+
+  def destroy
+    @report = @project.report
+    @report.destroy
+
+    redirect_to admin_project_report_url( @project )
+  end
+
 
   protected
 
@@ -10,8 +47,8 @@ class Admin::ProjectReportsController < ApplicationController
     @project = Project.find( params[:project_id] )
   end
   
-  def location_params
-    params.require(:location).permit(:name)
+  def report_params
+    params.require(:report).permit(:title, :content, :project_id)
   end
  
 end
