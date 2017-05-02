51cf3dd0c43342939b9d4899bfe84163bd9cc3fa
lyuchris <tw.chrislyu@gmail.com>
2017-05-02 16:51:32 +0800

    design user profile layout

--- app/controllers/account/project_donations_controller.rb
+++ app/controllers/account/project_donations_controller.rb
@@ -1,2 +1,7 @@
 class Account::ProjectDonationsController < ApplicationController
+	layout "account"
+	def index
+  	@user = current_user
+    @donations = Donation.where(:user_id => current_user.id)
+  end
 end
