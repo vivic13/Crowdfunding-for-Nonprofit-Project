bb04c7a9d1cfe1de3f4617930e7d3533d78efe02
Chen Jo Wei <meettheworldvivc@gmail.com>
2017-05-15 23:35:03 +0800

    change new pay2go info

--- app/controllers/account/project_donations_controller.rb
+++ app/controllers/account/project_donations_controller.rb
@@ -3,7 +3,7 @@ class Account::ProjectDonationsController < ApplicationController
 	
 	def index
 		@page_title = "我的贊助記錄"
-    @donations = Donation.where(:user_id => current_user.id).order('created_at desc').page(params[:page]).per(8)
+    @donations = Donation.where(:user_id => current_user.id).order('created_at desc')
   end
 
   def show 
