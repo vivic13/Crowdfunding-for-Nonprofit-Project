ae5a65f723fd339c3ff6b016eea3379c41143914
Chen Jo Wei <meettheworldvivc@gmail.com>
2017-05-16 19:57:54 +0800

    change environment setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://getlukcy.com" }
+  config.action_mailer.default_url_options = { host: "https://taiwanmega.life" }
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end
